2a93fff0ec599864d2c97c526f1e7f368d0c60cf
Ramiro Castillo <43893061+ramiroJCB@users.noreply.github.com>
2024-02-15 12:23:29 -0500

    chore(network): Adding test to verify public-ip validation and usage

--- cmd/gossamer/commands/init_test.go
+++ cmd/gossamer/commands/init_test.go
@@ -4,6 +4,8 @@
 package commands
 
 import (
+	"os/exec"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -15,11 +17,20 @@ const testChainSpec = "./test_inputs/test-chain-spec-raw.json"
 func TestInitFromChainSpec(t *testing.T) {
 	basepath := t.TempDir()
 
+	pubIp := "123.456.789.123"
 	rootCmd, err := NewRootCommand()
 	require.NoError(t, err)
 	rootCmd.AddCommand(InitCmd)
 
-	rootCmd.SetArgs([]string{InitCmd.Name(), "--base-path", basepath, "--chain", testChainSpec})
+	rootCmd.SetArgs([]string{InitCmd.Name(), "--base-path", basepath, "--chain", testChainSpec, "--public-ip", pubIp})
 	err = rootCmd.Execute()
 	require.NoError(t, err)
+
+	out, err := exec.Command("ping", pubIp).Output()
+	require.NoError(t, err)
+	if strings.Contains(string(out), "Destination Host Unreachable") {
+		t.Logf("ping %s failed", pubIp)
+	} else {
+		t.Log("public ip is reachable")
+	}
 }
